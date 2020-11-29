9e1ad79c3a266ce91f06ab094bb33c3918d9fcd9
Joey <joey7040@hotmail.com>
2020-11-29 14:03:55 -0500

    got real covid number need to fix type error on country change

---
+++ src/App.js
@@ -0,0 +1,100 @@
+
+import React, { useEffect, useState } from "react";
+import {MenuItem, FormControl, Select, Card, CardContent} from '@material-ui/core';
+import Infobox from './components/Infobox';
+import Map from './components/Map';
+import './App.css';
+
+
+// https://disease.sh/v3/covid-19/countries
+
+
+function App() {
+  const [country, setInputCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [countries, setCountries] = useState([]);
+
+
+  useEffect(()=>{
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data=>{
+      setCountryInfo(data);
+    })
+  },[])
+
+
+
+
+useEffect(()=>{
+  // runs one time when app loads
+  
+  const getCountriesData = async () => {
+   await fetch("https://disease.sh/v3/covid-19/countries")
+   .then((response) => response.json())
+   .then((data)=>{
+     const countries = data.map((country) => ({
+         name: country.country,
+         value: country.countryInfo.iso2
+       }));
+
+       setCountries(countries)
+   })
+  }
+  getCountriesData();
+},[])
+
+const onCountryChange = async (event) => {
+  const countryCode = event.target.value;
+
+  console.log("selected:" + countryCode);
+  setCountries(countryCode);
+
+  const url = countryCode === "worldwide" ? "https://disease.sh/v3/covid-19/all" : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+  await fetch(url)
+  .then((response)=>response.json())
+  .then((data)=>{
+    setInputCountry(countryCode);
+    setCountryInfo(data);
+    // setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+    // setMapZoom(4);
+  });
+};
+
+  return (
+    <div className="app">
+      <div className="app__left">
+        <div className='app__header'>
+          <h1>Covid-19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select variant='outlined' value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+              {countries.map((country)=>(
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+              <Infobox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+              <Infobox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+              <Infobox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+        </div>
+          <Map />
+        </div>
+        <Card className="app__right">
+          <CardContent>
+            <h3>Live Cases by Country</h3>
+            {/* table */}
+            <h3>WorldWide new cases</h3>
+
+          </CardContent>
+        </Card>
+    </div>
+  );
+}
+
+export default App;
